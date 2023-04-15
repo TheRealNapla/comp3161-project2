111c7b12110f9853aaeed434220dc6a512d67fed
Gabrielle Scott <scottgabrielle49@gmail.com>
2023-04-15 00:04:23 -0500

    gabrielle's tables

---
+++ admin.sql
@@ -0,0 +1,16 @@
+CREATE TABLE admin (admin_id INTEGER PRIMARY KEY,admin_name VARCHAR(255),admin_phonenumber int,email VARCHAR(255));
+INSERT INTO admin VALUES(admin_id ,admin_name, admin_phonenumber ,email)
+('861189',	'Gary Stewart',	'060-788-0463-790',	'ann66@example.org'),
+('836945',	'Kevin Contreras',	'(020)875-8270-53479',	'hardingsarah@example.org'),
+('298440',	'Wendy Munoz',	'313-360-5624',	'wesley30@example.net'),
+('499352',	'Kevin Lopez',	'080-898-7813-3230',	'craig07@example.net'),
+('327235',	'Michael Hernandez',	'(329)193-0046',	'james54@example.org'),
+('654252',	'Michelle Jordan',	'433-478-9899',	'kelly42@example.net'),
+('843915',	'Dennis Proctor', '012-371-5780',	'rtownsend@example.com'),
+('488794',	'Stephen Peterson',	'866-138-6011',	'curtiselizabeth@example.org'),
+('184937',	'Christopher Hanson',	'+1-051-924-2057',	'tiffanymartin@example.org'),
+('493167',	'Yvette Buck',	'708-194-8084-722',	'oschmidt@example.net');
+
+
+
+
